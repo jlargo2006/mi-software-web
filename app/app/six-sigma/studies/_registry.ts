d27d64f650700cdb02b40b933c9b415193a03b06
jlargo2006 <85826103+jlargo2006@users.noreply.github.com>
2026-07-28 14:36:28 +0200

    Update _registry.ts

--- app/app/six-sigma/studies/_registry.ts
+++ app/app/six-sigma/studies/_registry.ts
@@ -17,7 +17,7 @@ import pss1SampleT from "./pss1samplet";
 import pss2SampleT from "./pss/twosamplet";
 import pssAnova from "./pss/anova";
 import pssOneProportion from "./pss/oneproportion";
-import pssOneProportion from "./pss/twoproportion";
+import pssTwoProportion from "./pss/twoproportion";
 
 const ALL: ArtifactDefinition[] = [
   fishbone,
