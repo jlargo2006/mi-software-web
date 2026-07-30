dd9d28ea85ba0d1b951334fd887a3cd5fdc4183c
jlargo2006 <85826103+jlargo2006@users.noreply.github.com>
2026-07-30 07:59:58 +0200

    Fix import paths in Results.tsx

--- app/app/six-sigma/studies/pss1samplet/Results.tsx
+++ app/app/six-sigma/studies/pss1samplet/Results.tsx
@@ -3,10 +3,10 @@
 import React from "react";
 import type { Data } from "plotly.js";
 import type { ColumnSnapshot } from "../../types";
-import ResultChart from "../../../components/ResultChart";
-import ReportLayout from "../../../components/ReportLayout";
-import { niceBins } from "../../../lib/binning";
-import { percentile, buildContext, median } from "../../../lib/statistics";
+import ResultChart from "../../components/ResultChart";
+import ReportLayout from "../../components/ReportLayout";
+import { niceBins } from "../../lib/binning";
+import { percentile, buildContext, median } from "../../lib/statistics";
 import { ciStripTraces, ciStripLayout } from "../_shared/ciStrip";
 import { ALT_SYMBOL, type HT1SampleTParams, type HT1SampleTResult } from "./types";
 
