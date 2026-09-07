74a763929f682968e04a741907d4b2dad5727372
jlargo2006 <85826103+jlargo2006@users.noreply.github.com>
2026-09-07 09:37:16 +0200

    Update sitemap.ts

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,31 +3,31 @@ import type { MetadataRoute } from 'next'
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https://www.sixsigmamacrotools.com',
+      url: 'https://www.sixsigmaanalyzer.com',
       lastModified: new Date(),
       changeFrequency: 'weekly',
       priority: 1,
     },
     {
-      url: 'https://www.sixsigmamacrotools.com/login',
+      url: 'https://www.sixsigmaanalyzer.com/login',
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 0.8,
     },
     {
-      url: 'https://www.sixsigmamacrotools.com/privacy',
+      url: 'https://www.sixsigmaanalyzer.com/privacy',
       lastModified: new Date(),
       changeFrequency: 'yearly',
       priority: 0.5,
     },
     {
-      url: 'https://www.sixsigmamacrotools.com/terms',
+      url: 'https://www.sixsigmaanalyzer.com/terms',
       lastModified: new Date(),
       changeFrequency: 'yearly',
       priority: 0.5,
     },
     {
-      url: 'https://www.sixsigmamacrotools.com/contact',
+      url: 'https://www.sixsigmaanalyzer.com/contact',
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 0.6,
