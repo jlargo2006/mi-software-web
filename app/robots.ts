0c5b96bd915557ed8cd96e75f440745446aeb631
jlargo2006 <85826103+jlargo2006@users.noreply.github.com>
2026-09-07 09:34:40 +0200

    Update sitemap URL in robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -6,6 +6,6 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: "*",
       allow: "/",
     },
-    sitemap: "https://www.sixsigmamacrotools.com/sitemap.xml",
+    sitemap: "https://www.sixsigmaanalyzer.com/sitemap.xml",
   };
 }
